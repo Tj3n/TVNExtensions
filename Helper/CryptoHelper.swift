a8a2c0db5865e4ad442574ce48022b03e4de1b18
tien.vu <tien@paymentwall.com>
2018-08-08 10:00:02 +0700

    Update crypto

--- Helper/CryptoHelper.swift
+++ Helper/CryptoHelper.swift
@@ -6,18 +6,9 @@
 //
 
 import Foundation
-import CCommonCrypto
 
 public struct CryptoHelper {
-    public static func SHA256(data : Data) -> Data {
-        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA256($0, CC_LONG(data.count), &hash)
-        }
-        return Data(bytes: hash)
-    }
-    
-    public static func SHA256(with dict: [String: String], salt: String?) -> String {
+    public static func SHA256ToHex(with dict: [String: String], salt: String?) -> String {
         var keys = dict.keys.sorted()
         var signStr = ""
         for i in 0..<keys.count {
@@ -26,27 +17,11 @@ public struct CryptoHelper {
         if let salt = salt {
             signStr.append(salt)
         }
-        return hexStringFromData(input: SHA256(data: signStr.data(using: .utf8)!))
-    }
-    
-    public static func MD5(string: String) -> Data {
-        let messageData = string.data(using:.utf8)!
-        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
-        
-        _ = digestData.withUnsafeMutableBytes {digestBytes in
-            messageData.withUnsafeBytes {messageBytes in
-                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
-            }
-        }
-        
-        return digestData
+        return signStr.data(using: .utf8)!.SHA256().toHexString()
     }
     
     public static func MD5ToHex(string: String) -> String {
-        return hexStringFromData(input: MD5(string: string))
-    }
-    
-    private static func hexStringFromData(input: Data) -> String {
-        return input.map { String(format: "%02hhx", $0) }.joined()
+        let data = string.data(using: .utf8)!
+        return data.MD5().toHexString()
     }
 }
