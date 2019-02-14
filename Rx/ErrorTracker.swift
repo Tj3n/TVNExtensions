721899b2e737501bc618fbdc554c1fc8f77f3969
Tien Vu <tvn@planday.com>
2019-02-14 16:55:53 +0700

    Update some

---
+++ Rx/ErrorTracker.swift
@@ -0,0 +1,44 @@
+//
+//  ErrorTracker.swift
+//  TVNExtensions
+//
+//  Created by TienVu on 2/14/19.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ErrorTracker: SharedSequenceConvertibleType {
+    typealias SharingStrategy = DriverSharingStrategy
+    private let _subject = PublishSubject<Error>()
+    
+    func trackError<O: ObservableConvertibleType>(from source: O) -> Observable<O.E> {
+        return source.asObservable().do(onError: onError)
+    }
+    
+    func asSharedSequence() -> SharedSequence<SharingStrategy, Error> {
+        return _subject.asObservable().asDriver(onErrorRecover: { (_) in
+            return Driver.empty()
+        })
+    }
+    
+    func asObservable() -> Observable<Error> {
+        return _subject.asObservable()
+    }
+    
+    private func onError(_ error: Error) {
+        _subject.onNext(error)
+    }
+    
+    deinit {
+        _subject.onCompleted()
+    }
+}
+
+extension ObservableConvertibleType {
+    func trackError(_ errorTracker: ErrorTracker) -> Observable<E> {
+        return errorTracker.trackError(from: self)
+    }
+}
+
