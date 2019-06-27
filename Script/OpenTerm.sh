6fe1539271924ccc683db592a6074b50e331266c
Tien Vu <tvn@planday.com>
2019-06-27 09:15:43 +0700

    Add terminal script

---
+++ Script/OpenTerm.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+osascript <<EOD
+tell application "Xcode"
+	set currentWorkspacePath to path of active workspace document
+end tell
+
+if application "Terminal" is running then 
+    tell application "Terminal"
+    	activate
+		delay 0.5
+        tell application "System Events" to keystroke "t" using {command down}    
+        do script "cd " & currentWorkspacePath & "/.." in front window                           
+    end tell                              
+else                                      
+    tell application "Terminal"
+    	do script "cd " & currentWorkspacePath & "/.." in front window
+        activate
+    end tell
+end if
+EOD
