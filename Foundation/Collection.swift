16c6920b20435475115f1a68a9a76d4938275573
Tien Vu <tien.vu@nanosurvey.com>
2020-02-06 10:06:08 +0700

    Update

--- Foundation/Collection.swift
+++ Foundation/Collection.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-extension Collection {
+public extension Collection {
     
     /// Returns the element at the specified index iff it is within bounds, otherwise nil.
     subscript (safe index: Index) -> Iterator.Element? {
@@ -16,9 +16,15 @@ extension Collection {
     }
 }
 
-extension Array {
+public extension Array {
     func write(to url: URL, atomically: Bool = true) -> Bool{
         let array = self as NSArray
         return array.write(to: url, atomically: atomically)
+    }
+    
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
     }
 }
